3b3ab49ceff70da9c19c9f11b2232f7b8abd8bc9
undeadskeleton <safepeople@gmail.com>
2025-03-18 13:29:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// Declare an empty array;
+let emArr = [];
+
+console.log(`Empty array: ${emArr}`);
+
+// Declare an array with more than 5 number of elements
+emArr = ["atom", 2, 35, [2.5, 2354]];
+
+console.log(
+  `Different type of value in array ${emArr.map((str) => str.concat(" "))}`
+);
+
+// Find the length of your array
+// Get the first item, the middle item and the last item of the array
+// Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+// Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+// Print the array using console.log()
+// Print the number of companies in the array
+// Print the first company, middle and last company
+// Print out each company
+// Change each company name to uppercase one by one and print them out
+// Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+// Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
+// Filter out companies which have more than one 'o' without the filter method
+// Sort the array using sort() method
+// Reverse the array using reverse() method
+// Slice out the first 3 companies from the array
+// Slice out the last 3 companies from the array
+// Slice out the middle IT company or companies from the array
+// Remove the first IT company from the array
+// Remove the middle IT company or companies from the array
+// Remove the last IT company from the array
+// Remove all IT companies
+// Exercise: Level 2
+// Create a separate countries.js file and store the countries array into this file, create a separate file web_techs.js and store the webTechs array into this file. Access both file in main.js file
