7ada98868af94f4e050dcbacf49681f4c55001a0
undeadskeleton <safepeople@gmail.com>
2025-03-26 19:47:09 +0530

    starting functional programming exercise

--- Day1/Functional__Programming/scipt.js
+++ Day1/Functional__Programming/scipt.js
@@ -35,3 +35,34 @@ const odd = numbers2.filter((num) => num % 2 !== 0);
 const even = numbers2.filter((num) => num % 2 === 0);
 
 console.log(`The even numbers are ${even} and odd numbers are ${odd}`);
+
+//reduce
+
+const numbers3 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const concatnumber = numbers3.reduce((acc, cur) => acc + " " + cur, 6);
+
+console.log(concatnumber);
+
+const number4 = [1, 2, 3, 4, 5];
+
+const value = number4.reduce((acc, cur) => acc * cur, 5);
+
+console.log(value);
+
+//find
+
+const sixCharCountry = countries.find((country) => country.length === 6);
+
+console.log(sixCharCountry);
+//some
+
+const evens = [0, 2, 4, 6, 8, 10];
+
+const someAreEven = even.some((n) => n % 2 === 0);
+
+console.log(`The array of even has some even numbers: ${someAreEven}`);
+//every
+
+const someAreOdd = even.every((n) => n % 2 !== 0);
+console.log(`The array of even has every odd numbers: ${someAreOdd}`);
