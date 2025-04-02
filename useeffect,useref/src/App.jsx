cec170a190feb4202519bf58a3a78cc9eb84f494
undeadskeleton <safepeople08@gmail.com>
2025-04-02 17:44:36 +0530

    finished video of password creator project

--- useeffect,useref/src/App.jsx
+++ useeffect,useref/src/App.jsx
@@ -1,35 +1,107 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [characterAllowed, setCharaterAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  //ref hook
+  const passwordRef = useRef(null);
+
+  const passwordGenrator = useCallback(() => {
+    let pass = "";
+    let str = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+
+    if (numberAllowed) {
+      str += "0123456789";
+    }
+    if (characterAllowed) {
+      str += "!@#$%&*-_,.";
+    }
+
+    for (let i = 0; i < length; i++) {
+      let randomChar = Math.random() * str.length;
+      pass += str.charAt(randomChar);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, characterAllowed, setPassword]);
+
+  const copyPasswordToClicboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  useEffect(() => {
+    passwordGenrator();
+  }, [length, numberAllowed, characterAllowed, passwordGenrator]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-4 my-8  text-white text-2xl bg-gray-700">
+        <h1 className="text-white text-center">Password Generator</h1>
+
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-3 bg-white text-black pl-3"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+          />
+
+          <button
+            onClick={copyPasswordToClicboard}
+            className="ouline-none bg-blue-700 text-white px-3 py-1 shrink-0 cursor-pointer"
+          >
+            Copy
+          </button>
+        </div>
+
+        <div className="flex text-sm gap-x-2">
+          <div className="flex intems-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={15}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label>Length:{length}</label>
+          </div>
+
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label>Number</label>
+          </div>
+
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={characterAllowed}
+              id="characterInput"
+              onChange={() => {
+                setCharaterAllowed((prev) => !prev);
+              }}
+            />
+            <label>Character</label>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
