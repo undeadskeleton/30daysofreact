6eac2b84c75ff412f082425089106dd9df8adb2d
undeadskeleton <safepeople08@gmail.com>
2025-04-02 21:17:50 +0530

    fuck it we ball

---
+++ chaiaurcodejavascript/Datatype_and_ECMA/script.js
@@ -0,0 +1,10 @@
+"use strict"; //treat all JS code as newer  version
+
+// alert("hello"); //we are using nodejs, not browser so it wont work
+
+//symbol =>unique
+
+//object
+console.log(typeof { age: 12, reginon: "latyus" });
+console.log(typeof undefined);
+console.log(typeof null);
