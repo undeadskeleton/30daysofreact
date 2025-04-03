2874133c1c8b7328e07d6d89752500c07e1653f4
undeadskeleton <safepeople08@gmail.com>
2025-04-03 11:20:29 +0530

    Conversion of data type using Number(),Boolean(),String()

--- chaiaurcodejavascript/Datatype_conversion_confusin/script.js
+++ chaiaurcodejavascript/Datatype_conversion_confusin/script.js
@@ -1,7 +1,34 @@
-let score = "33";
+let score = undefined;
 
 console.log(typeof score);
 console.log(typeof score);
 
 let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//Number conversion with Number()
+//"333" => 333
+// "333cc" => NaN
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = () => {
+  return true;
+};
+
+let bolleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(bolleanIsLoggedIn);
+
+//Boolen conversion with Boolean()
+//"333" => true
+// "333cc" => true
+//0 => false
+// "" => false
+// null => false
+// undefined => false
+// NaN => false
+//[]=> true
+//{}=> true
