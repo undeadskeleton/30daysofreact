0b0115d41716da24e47f67b4220663e988f7020d
undeadskeleton <safepeople08@gmail.com>
2025-04-15 15:01:06 +0530

    problem with swap function

--- curencycalculator/src/App.jsx
+++ curencycalculator/src/App.jsx
@@ -6,28 +6,61 @@ import useCurrencyInfo from "./assets/hooks/useCurrencyInfo";
 function App() {
   const [from, setFrom] = useState("usd");
   const [to, setTo] = useState("inr");
+  const [amount, setAmount] = useState("");
+  const [convertedAmount, setConvertedAmount] = useState(0);
 
   const currencyInfo = useCurrencyInfo(from);
   const options = Object.keys(currencyInfo);
+
+  const convert = () => setConvertedAmount(amount * currencyInfo[to]);
+
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  };
 
   return (
     <>
       <h1 className="text-2xl bg-yellow-500 py-5">Currency Converter</h1>
       <div>
-        <div>
-          <InputBox label="From" options={options} selectedCurrency={from} />
-        </div>
-        <div>
-          <button>Swap</button>
-        </div>
-        <div>
-          <InputBox label="To" options={options} selectedCurrency={to} />
-        </div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            convert();
+          }}
+        >
+          <div>
+            <InputBox
+              label="From"
+              options={options}
+              amount={amount}
+              selectedCurrency={from}
+              onAmountChange={(amount) => {
+                return setAmount(amount);
+              }}
+              onCurrencyChange={(currency) => setFrom(currency)}
+            />
+          </div>
+          <div>
+            <button onClick={swap}>Swap</button>
+          </div>
+          <div>
+            <InputBox
+              label="To"
+              options={options}
+              selectedCurrency={to}
+              amount={convertedAmount}
+              changeDisabled
+              onCurrencyChange={(currency) => setTo(currency)}
+            />
+          </div>
+          <button type="submit">
+            Convert {from.toUpperCase()} to {to.toUpperCase()}{" "}
+          </button>
+        </form>
       </div>
-      <button>
-        Convert {from.toUpperCase()} to {to.toUpperCase()}{" "}
-      </button>
-      <div></div>
     </>
   );
 }
--- curencycalculator/src/assets/components/InputBox.jsx
+++ curencycalculator/src/assets/components/InputBox.jsx
@@ -1,8 +1,16 @@
 import { useState, useEffect, useId } from "react";
 import "./InputBox.css";
 
-function InputBox({ label, options, selectedCurrency, onAmountChange }) {
+function InputBox({
+  label,
+  options,
+  onAmountChange,
+  changeDisabled = false,
+  amount,
+  onCurrencyChange,
+}) {
   const currId = useId();
+  const [selectedCurrency, setSelectedCurrency] = useState("inr");
 
   return (
     <div className="flex justify-center gap-10 border-2 ">
@@ -11,13 +19,22 @@ function InputBox({ label, options, selectedCurrency, onAmountChange }) {
         <input
           className="outline-none w-full"
           type="number"
-          onChange={(e) => onAmountChange(e.target.value)}
+          onChange={(e) => onAmountChange(Number(e.target.value))}
+          value={amount}
+          disabled={changeDisabled}
         />
-        {console.log(onAmountChange)}
       </div>
       <div className="flex flex-col">
         <label htmlFor="">Currency</label>
-        <select name="" id="" value={selectedCurrency}>
+        <select
+          name=""
+          id=""
+          value={selectedCurrency}
+          onChange={(e) => {
+            console.log("The currency is :", onCurrencyChange(e.target.value));
+            return onCurrencyChange && onCurrencyChange(e.target.value);
+          }}
+        >
           {options.map((curr) => (
             <option value={curr} key={curr}>
               {curr}
